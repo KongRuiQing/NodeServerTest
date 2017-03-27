ac6544b5ee2ce943df077d22abbff173efbd58b7
kongruiqing <news080@yeah.net>
2017-03-28 07:07:50 +0800

    修改json

--- RestfulApi/index.js
+++ RestfulApi/index.js
@@ -21,11 +21,12 @@ http_header[500] = 'text/plain';
 var http_obj = {};
 
 http_obj['/admin/v1/ad'] = require('./API/AdRestFulApi.js').Instance();
+
 let PORT = 0;
 function handle_route(request,response,next){
 	var pathname = url.parse(request.url).pathname;
 	var method = request.method.toUpperCase();
-
+	logger.log("INFO",pathname);
 	if(pathname in http_obj){
 		
 		if(http_obj[pathname].emit(method,request,response)){
@@ -43,17 +44,20 @@ function handle_route(request,response,next){
 
 function handle_Error(err, req, res, next){
 	logger.error("POST_SERVER",err);
-	res.writeHead(500, {
-		'Content-Type': http_header[500]
+	res.writeHead(200, {
+		'Content-Type': http_header[200]
 	});
-	res.end();
+	res.end(JSON.stringify({
+		'error' : 'pathname is error',
+	}));
 }
 
 exports.start = function(Host,Port){
 	PORT = Port;
 	var app = connect()
 	.use(favicon("favicon.ico"))
-	.use(bodyParser.urlencoded({ extended: false }))
+	//.use(bodyParser.urlencoded({ extended: false }))
+	.use(bodyParser.json())
 	.use(handle_route)
 	.use(handle_Error)
 	app.listen(Port);
