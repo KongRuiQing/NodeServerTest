f3f69caa381f670815da6931418c02ab17cb341f
kongruiqing <news080@yeah.net>
2017-03-29 06:01:26 +0800

    JSON

--- RestfulApi/index.js
+++ RestfulApi/index.js
@@ -65,8 +65,8 @@ exports.start = function(Host,Port){
 	PORT = Port;
 	var app = connect()
 	.use(favicon("favicon.ico"))
-	.use(bodyParser.urlencoded({ extended: false }))
-	//.use(bodyParser.json())
+	//.use(bodyParser.urlencoded({ extended: false }))
+	.use(bodyParser.json())
 	.use(handle_head)
 	.use(handle_route)
 	.use(handle_Error)
