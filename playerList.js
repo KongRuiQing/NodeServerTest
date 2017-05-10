4ddb577fc65beb54243bcd6dd27d4bbfbc5988af
kongruiqing <news080@yeah.net>
2017-05-11 06:38:40 +0800

    临时使用1234

--- playerList.js
+++ playerList.js
@@ -261,6 +261,7 @@ function GetRandomNum()
 }   
 
 function generateVerifyCode(){
+	return "1234";
 	var chars = ['0','1','2','3','4','5','6','7','8','9'];
 	var result = "";
 	for(var i = 0; i < 4 ; i ++){
