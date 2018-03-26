75a07c8d817d87be1a414221cef26ea1fe970e19
kongruiqing <news080@yeah.net>
2018-03-26 21:34:06 +0800

    3

--- WebSocketServer/index.js
+++ WebSocketServer/index.js
@@ -184,7 +184,7 @@ exports.start = function(HOST, PORT) {
 
 exports.bind = function(socket_id,uid){
 	if(app != null){
-		let client = this.clients.get(socket_id);
+		let client = app.clients.get(socket_id);
 		if(client){
 			client.uid = uid;
 		}
