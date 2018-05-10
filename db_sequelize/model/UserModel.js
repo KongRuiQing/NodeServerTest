9b455a84a0bbe549104d4a124b3d2c7fb103a2b4
kongruiqing <news080@yeah.net>
2018-05-10 21:38:51 +0800

    添加姓别

--- db_sequelize/model/UserModel.js
+++ db_sequelize/model/UserModel.js
@@ -1,44 +1,48 @@
 'use strict';
 module.exports = function (sequelize, DataTypes) {
   return sequelize.define('user', {
-    'id':{
-      type:DataTypes.BIGINT(11), 
-      autoIncrement:true, 
-      primaryKey : true, 
-      unique : true
+    'id': {
+      type: DataTypes.BIGINT(11),
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true
     },
-    'head' : {
-      type:DataTypes.STRING,
-      defaultValue:"player/default.png"
+    'head': {
+      type: DataTypes.STRING,
+      defaultValue: "player/default.png"
     },
-    'name' : {
-      type : DataTypes.STRING,
-      defaultValue : '用户'
+    'name': {
+      type: DataTypes.STRING,
+      defaultValue: '用户'
     },
-    'sign' : {
-      type : DataTypes.STRING,
-      defaultValue:"",
+    'sign': {
+      type: DataTypes.STRING,
+      defaultValue: "",
     },
-    'address' : {
-      type : DataTypes.STRING,
-      defaultValue:"",
+    'address': {
+      type: DataTypes.STRING,
+      defaultValue: "",
     },
-    'telephone' : {
-      type : DataTypes.STRING,
-      defaultValue:"",
+    'telephone': {
+      type: DataTypes.STRING,
+      defaultValue: "",
     },
-    'email':{
-      type : DataTypes.STRING,
-      defaultValue:"",
+    'email': {
+      type: DataTypes.STRING,
+      defaultValue: "",
     },
-    'birthday_timestamp' : {
-      type : DataTypes.DATE,
+    'birthday_timestamp': {
+      type: DataTypes.DATE,
     },
-    'real_name' : {
-      type : DataTypes.STRING,
-      defaultValue : "",
+    'real_name': {
+      type: DataTypes.STRING,
+      defaultValue: "",
+    },
+    'real_name': {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
     }
-  },{
-    'tableName' : 'user_info',
+  }, {
+    'tableName': 'user_info',
   });
 };
